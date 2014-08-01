73bc9ce8bd2787a89d8404299e4459980f89dbcc
jjconti <jjconti@gmail.com>
2014-08-01 12:49:07 -0300

    change non_empty? method in lib/regexp_m17n.rb to make test pass

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,11 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    pattern = '^.+$'
+    if Encoding.compatible?(str, pattern)
+      regex = Regexp.new(pattern.force_encoding(str.encoding), Regexp::FIXEDENCODING)
+    else
+      regex = Regexp.new(pattern.encode(str.encoding), Regexp::FIXEDENCODING)
+    end
+    str =~ regex
   end
 end
